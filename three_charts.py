b7d60b2359d4e196c540b534d6c68ec907cd1753
crf61 <crf61@georgetown.edu>
2020-02-10 17:41:53 -0500

    I copied and pasted the data under "usage"

--- three_charts.py
+++ three_charts.py
@@ -50,3 +50,25 @@ bar_data = [
 print("----------------")
 print("GENERATING BAR CHART...")
 print(bar_data) # TODO: create a horizontal bar chart based on the bar_data
+
+# adapted from: https://plot.ly/python/getting-started/#initialization-for-offline-plotting
+
+import plotly
+import plotly.graph_objs as go
+
+plotly.offline.plot({
+    "data": [go.Scatter(x=[1, 2, 3, 4], y=[4, 3, 2, 1])],
+    "layout": go.Layout(title="hello world")
+}, auto_open=True)
+
+# adapted from: https://plot.ly/python/pie-charts/
+
+import plotly
+import plotly.graph_objs as go
+
+labels = ["Oxygen", "Hydrogen", "Carbon_Dioxide", "Nitrogen"]
+values = [4500, 2500, 1053, 500]
+
+trace = go.Pie(labels=labels, values=values)
+
+plotly.offline.plot([trace], filename="basic_pie_chart.html", auto_open=True)
